175dddd0b07d7b3d3a2f50e4b33841657b6c2d6a
Danny-Sullivan <dsully360@gmail.com>
2015-11-12 13:15:22 -0500

    added find_by_id, where, count, and first type methods

--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -9,9 +9,10 @@ describe Student do
   after do
     Student.drop_table
   end
-
   let(:pat) {Student.new}
   let(:sam) {Student.new}
+  let(:jess) {Student.new}
+
   let(:attributes) {
     {
       :id => 1,
@@ -20,7 +21,7 @@ describe Student do
     }
   }
 
-  describe 'attributes' do 
+  describe 'attributes' do
     it 'has an id, name, grade' do
       pat.id = attributes[:id]
       pat.name = attributes[:name]
@@ -52,8 +53,8 @@ describe Student do
     end
   end
 
-  describe "#save" do 
-    it 'saves an instance of the Student class to the database' do 
+  describe "#save" do
+    it 'saves an instance of the Student class to the database' do
       pat.save
       expect(DB[:conn].execute("SELECT * FROM students")).to eq([[1, nil, nil]])
     end
@@ -70,7 +71,7 @@ describe Student do
     end
   end
 
-  context 'retrieving data from the db' do 
+  context 'retrieving data from the db' do
     describe '.find_by_name' do
 
       it 'returns an instance of student that matches the name from the DB' do
@@ -84,8 +85,21 @@ describe Student do
       end
     end
 
-    describe '.all' do 
-      it 'returns all student instances from the db' do 
+    describe '.find_by_id' do
+
+      it 'returns an instance of student that matches the id from the DB' do
+        pat.name = "Pat"
+        pat.id = 1
+        pat.save
+
+        pat_from_db = Student.find_by_id(1)
+        expect(pat_from_db.id).to eq(1)
+        expect(pat_from_db).to be_an_instance_of(Student)
+      end
+    end
+
+    describe '.students_below_12th_grade' do
+      it 'returns an array of all students in grades 11 or below' do
         pat.name = "Pat"
         pat.grade = 12
         pat.save
@@ -93,12 +107,63 @@ describe Student do
         sam.grade = 10
         sam.save
 
-        all_from_db = Student.all 
+        all_but_12th = Student.students_below_12th_grade
+        expect(all_but_12th.size).to eq(1)
+      end
+    end
+
+    describe '.first_student_in_grade_12' do
+      it 'returns the first student in grade 12' do
+        pat.name = "Pat"
+        pat.grade = 12
+        pat.id = 1
+        pat.save
+
+        sam.name = "Sam"
+        sam.grade = 10
+        sam.id = 2
+        sam.save
+
+        jess.name = "Jess"
+        jess.grade = 10
+        jess.id = 3
+        jess.save
+
+        first_student = Student.first_student_in_grade_10
+        expect(first_student.id).to eq(2)
+        expect(first_student.name).to eq("Sam")
+      end
+    end
+
+    describe '.count_all_students_in_grade_9' do
+      it 'returns an array of all students in grades 9' do
+        pat.name = "Pat"
+        pat.grade = 12
+        pat.save
+        sam.name = "Sam"
+        sam.grade = 9
+        sam.save
+
+        all_in_9 = Student.count_all_students_in_grade_9
+        expect(all_in_9.size).to eq(1)
+      end
+    end
+
+    describe '.all' do
+      it 'returns all student instances from the db' do
+        pat.name = "Pat"
+        pat.grade = 12
+        pat.save
+        sam.name = "Sam"
+        sam.grade = 10
+        sam.save
+
+        all_from_db = Student.all
         expect(all_from_db.size).to eq(2)
         expect(all_from_db.last).to be_an_instance_of(Student)
-        expect(all_from_db.last.name).to eq("Sam")         
+        expect(all_from_db.last.name).to eq("Sam")
       end
     end
   end
-  
+
 end
